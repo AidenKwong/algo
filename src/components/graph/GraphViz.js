52424e28a006b626d796493da340c48326f52a34
Kwong Aiden <aidenkwong5@gmail.com>
2022-02-23 19:24:42 +0800

    try fix build error

--- src/components/graph/GraphViz.js
+++ src/components/graph/GraphViz.js
@@ -1,5 +1,5 @@
 import "./GraphViz.scss";
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useRef } from "react";
 import constructGraph from "./constructGraph";
 
 const GraphViz = () => {
