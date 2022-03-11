b556e6758b8f3b0d1e22c71c8c0990303b4d95ce
Kwong Aiden <aidenkwong5@gmail.com>
2022-03-12 03:13:30 +0800

    add linked list

--- src/components/linked_list/LinkedListViz.jsx
+++ src/components/linked_list/LinkedListViz.jsx
@@ -1,23 +1,134 @@
 import React, { useState, useEffect, useRef } from "react";
 import LinkedList from "./linked_list";
+import * as echarts from "echarts";
+import { SVGRenderer, CanvasRenderer } from "echarts/renderers";
+import styles from "./LinkedListViz.module.scss";
+
+const timer = (ms) => new Promise((res) => setTimeout(res, ms));
+
+var myChart = echarts.extendComponentModel;
+echarts.use([SVGRenderer, CanvasRenderer]);
+const maxLength = 10;
 
 const LinkedListViz = () => {
   const graphRef = useRef();
+  const [number, setNumber] = useState(0);
+  const [running, setRunning] = useState(false);
   const [list, setList] = useState(new LinkedList(0));
+  const [output, setOutput] = useState([]);
+  const [outputLink, setOutputLink] = useState([]);
+  const [constructed, setConstructed] = useState(false);
 
   useEffect(() => {
-    var tempList = list;
-    for (let i = 0; i < 10; i++) {
-      tempList.add(Math.ceil(Math.random() * 100));
+    myChart = echarts.init(graphRef.current, null, {
+      renderer: "svg",
+    });
+  }, []);
+
+  useEffect(() => {
+    myChart.setOption({
+      series: [
+        {
+          type: "graph",
+          layout: "none",
+          symbolSize: 40,
+          silent: true,
+          symbol: "rect",
+          lineStyle: {
+            color: "#3d405b",
+            width: 4,
+            opacity: 0.75,
+            zLevel: 2,
+          },
+          itemStyle: {
+            borderColor: "#3d405b",
+            borderWidth: 3,
+            color: "white",
+          },
+          label: {
+            show: true,
+            fontSize: 16,
+            fontWeight: 600,
+            // offset: [-5, 0],
+          },
+          edgeSymbol: ["none", "arrow"],
+          data: output,
+          // links: [],
+          links: [],
+        },
+      ],
+    });
+  }, []);
+
+  const handleRun = (e) => {
+    e.preventDefault();
+    setRunning(true);
+    setList(new LinkedList(0));
+    if (constructed) {
+      setOutput([]);
+      setOutputLink([]);
+      setRunning(false);
+      return setConstructed(false);
     }
 
-    setList(tempList);
-  }, [list]);
+    var tempList = list;
+    const iterate = async () => {
+      for (let i = 0; i < number; i++) {
+        let random = Math.ceil(Math.random() * 10);
+        tempList.add(random);
+        await timer(1000);
+        setList(tempList);
+        setOutput((prevState) => {
+          prevState.push({
+            id: i,
+            name: `${random}`,
+            x: i * 30,
+            y: 50,
+          });
+          return prevState;
+        });
+
+        if (output.length > 1)
+          setOutputLink((prevState) => {
+            prevState.push({
+              source: output[i - 1]?.id,
+              target: output[i]?.id,
+            });
+            return prevState;
+          });
+
+        myChart.setOption({ series: [{ data: output, links: outputLink }] });
+      }
+      setRunning(false);
+      setConstructed(true);
+    };
+    iterate();
+  };
+
   return (
     <>
       <h3>In Progress...</h3>
-      <pre>{JSON.stringify(list, null, 2)}</pre>
-      <div ref={graphRef} style={{ width: "100%", height: "100%" }} />
+      <form onSubmit={handleRun}>
+        <div className={styles.buildPrompt}>
+          <p>
+            {`Enter the number of nodes you want to contruct the random graph ( < ${maxLength} )`}
+          </p>
+          <div className={styles.inputButtonGroup}>
+            <input
+              onChange={(e) => setNumber(e.target.value)}
+              placeholder={`< ${maxLength}`}
+            />
+            <button disabled={running | (number > maxLength)}>
+              {running ? <div className="RUNNING" /> : "BUILD"}
+            </button>
+          </div>
+        </div>
+      </form>
+      <div
+        className={styles.vizContainer}
+        ref={graphRef}
+        style={{ width: "100%", height: "50vh" }}
+      />
     </>
   );
 };
